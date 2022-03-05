a8b69b794a80970a28ac8fb4e6cbb87e1851c96d
xapis-21 <78486475+xapis-21@users.noreply.github.com>
2022-03-05 18:51:03 +0300

    first commit

---
+++ backend/schemas/loanee.js
@@ -0,0 +1,47 @@
+export default {
+    name: 'loanee',
+    title: 'Loanee',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'loanAmount',
+            title: 'LoanAmount',
+            type: 'string'
+        },
+        {
+            name: 'fee',
+            title: 'Fee',
+            type: 'string'
+        },
+        {
+            name: 'refree',
+            title: 'Refree',
+            type: 'string'
+        },
+        {
+            name: 'refreeNumber',
+            title: 'RefreeNumber',
+            type: 'string'
+        },
+        {
+            name: 'postedy',
+            title: 'PostedBy',
+            type: 'string'
+        },
+        {
+            name: 'close',
+            title: 'Close',
+            type: 'string'
+        },
+        {
+            name: 'status',
+            title: 'Status',
+            type: 'string'
+        }
+    ]
+}
